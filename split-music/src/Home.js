719598f7e09f1ce7231f98b0ceb0daaefce40ccf
Q Hardy <qhardy1@gmail.com>
2019-02-07 11:19:09 -0500

    css updates

--- split-music/src/Home.js
+++ split-music/src/Home.js
@@ -1,18 +1,23 @@
 import React, { Component,PropTypes } from 'react';
+import './App.css';
 import ReactDOM from 'react-dom'
 import ReactPlayer from 'react-player'
+import './Allmusic.css'
 import {Link} from 'react-router-dom'
+import OdeszaLoyal from './Tracks/OdeszaLoyal.js'
+import XieDrip from './Tracks/XieDrip.js'
 
-class House extends Component {
+class Home extends Component {
   render() {
     return (
-        <div>
-        <p> House Test</p>
+        <div className='Allmusic'>
+        <div className='tracks'>
+        <XieDrip />
+        <OdeszaLoyal />
         </div>
-
-       
+        </div>
     )
   }
 }
 
-export default House;
+export default Home;
---
+++ split-music/src/House.js
@@ -0,0 +1,18 @@
+import React, { Component,PropTypes } from 'react';
+import ReactDOM from 'react-dom'
+import ReactPlayer from 'react-player'
+import {Link} from 'react-router-dom'
+
+class House extends Component {
+  render() {
+    return (
+        <div>
+        <p> House Test</p>
+        </div>
+
+       
+    )
+  }
+}
+
+export default House;
