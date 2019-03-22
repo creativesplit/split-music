63ac9f69bab7cc6146800dca3ddbc58f28663ab7
Q Hardy <qhardy1@gmail.com>
2019-03-22 14:23:44 -0400

    animation for side drawer and backdrop

--- split-music/src/Header1.js
+++ split-music/src/Header1.js
@@ -8,39 +8,17 @@ import  DrawerToggleButton from './DrawerToggleButton'
 
 const Header1 = props => (
 
-        <div>
+    <div>
         <header className='header-wrapper'> 
-        <img className='logo' src="https://i.imgur.com/ya49W4U.jpg" height='50' width='70'></img>
-        <h1 className='company-name'>SPLIT MUSIC <br/> <h3 className='tagline'>the best new music without an agenda</h3></h1>
-        <nav className='header-nav'>
-          <div className='hamburger-button'>
-            <DrawerToggleButton click={props.drawerClickHandler}/>
-          </div>
-            <div className='header-nav-items'>
-            <ul>
-              <li><a className='header-link' type='button' href='/hiphop'>Hip Hop</a></li>
-              <li><a className='header-link' type='button' href='/house'>House</a></li>
-              <li><a className='header-link' type='button' href='/trap'>Trap</a></li>
-              <li><a className='header-link' type='button' href='/progressivehouse'>Progressive House</a></li>
-              <li><a className='header-link' type='button' href='/electropop'> Electro Pop</a></li>
-              <li><a className='header-link' type='button' href='/heavy'>Heavy</a></li>
-              <li><a className='header-link' type='button' href='/other'>Other</a></li>
-              <li><a  className='header-link'type='button' href='/'>Home</a></li>
-            </ul>
-          </div> 
-        </nav>
-      
-      </header>
+            <img className='logo' src="https://i.imgur.com/ya49W4U.jpg" height='50' width='70'></img> 
+            <h1 className='company-name'>SPLIT MUSIC <br/> <h3 className='tagline'>the best music without an agenda</h3></h1>
+            <nav className='header-nav'>
+                <div className='hamburger-button'>
+                    <DrawerToggleButton click={props.drawerClickHandler}/>
+                </div>
+            </nav>
+        </header>
       </div>
     )
 
 export default Header1;
-
-
-{/* <Link className='header-link' type='button' to='/'>All Music</Link>
-<Link className='header-link' type='button' to='/hiphop'>Hip Hop</Link>
-<Link className='header-link' type='button' to='/house'>House</Link>
-<Link className='header-link' type='button' to='/trap'>Trap</Link>
-<Link className='header-link' type='button' to='/progressivehouse'>Progressive House</Link>
-<Link className='header-link' type='button' to='/electropop'> Electro Pop</Link>
-<Link className='header-link' type='button' to='/'>Home</Link> */}
